4ece547d5a1fa023290980aad5ad30556b70dc83
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-12-28 02:22:47 +0000

    Hello World Tails::Application

--- lib/tails.rb
+++ lib/tails.rb
@@ -3,5 +3,9 @@
 require 'tails/version'
 
 module Tails
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, { 'Content-Type' => 'text/html' }, ['Hello from Ruby on Tails!']]
+    end
+  end
 end
