94cc3ebbf13de9901d250a6f7065a0dbc96727e4
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-12-29 17:10:28 +0000

    Add special cases for favicon.ico and root

--- lib/tails.rb
+++ lib/tails.rb
@@ -6,11 +6,41 @@ require 'tails/routing'
 module Tails
   class Application
     def call(env)
+      return not_found if favicon_request?(env)
+      return default_response if root_request?(env)
+
       router = Routing.new
       klass, action = router.get_controller_and_action(env)
+
       controller = klass.new(env)
       body = controller.public_send(action.to_sym)
+      ok body
+    end
+
+    private
+
+    def ok(body)
       [200, { 'Content-Type' => 'text/html' }, [body]]
+    end
+
+    def not_found
+      [404, { 'Content-Type' => 'text/html' }, []]
+    end
+
+    def redirect(location)
+      [302, { 'Location' => location }, []]
+    end
+
+    def default_response
+      ok 'Welcome to Tails! Build your app by adding your own controllers.'
+    end
+
+    def favicon_request?(env)
+      env['PATH_INFO'] == '/favicon.ico'
+    end
+
+    def root_request?(env)
+      env['PATH_INFO'] == '/'
     end
   end
 
