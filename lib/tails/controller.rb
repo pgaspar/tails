ef059c8f542385fd116700afb96b0ec8354f4971
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-12-31 02:32:54 +0000

    Namespace views using controller name

--- lib/tails/controller.rb
+++ lib/tails/controller.rb
@@ -9,10 +9,16 @@ module Tails
     end
 
     def render(view_name, locals = {})
-      filename = File.join 'app', 'views', "#{view_name}.html.erb"
+      filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(env: env)
+    end
+
+    def controller_name
+      klass = self.class
+      klass_name = klass.to_s.gsub(/Controller$/, '')
+      Tails.to_underscore klass_name
     end
   end
 end
