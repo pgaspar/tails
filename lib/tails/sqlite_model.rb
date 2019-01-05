9f2f3094315c147ddb8c4cad141a2566788ac4a5
Pedro Gaspar <pedro.gaxpar@gmail.com>
2019-01-05 14:19:17 +0000

    Allow creating and counting Model::SQLite rows

--- lib/tails/sqlite_model.rb
+++ lib/tails/sqlite_model.rb
@@ -7,6 +7,44 @@ DB = SQLite3::Database.new DB_NAME
 module Tails
   module Model
     class SQLite
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+        when NilClass
+          'null'
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
+      def self.create(values)
+        values.delete 'id'
+        keys = schema.keys - ['id']
+        vals = keys.map { |k| to_sql(values[k]) }
+
+        DB.execute <<~SQL
+          INSERT INTO #{table} (#{keys.join(',')})
+            VALUES (#{vals.join(',')});
+        SQL
+
+        raw_vals = keys.map { |k| values[k] }
+        data = Hash[keys.zip raw_vals]
+        sql = 'SELECT last_insert_rowid();'
+        data['id'] = DB.execute(sql)[0][0]
+        new(data)
+      end
+
+      def self.count
+        DB.execute("SELECT COUNT(*) FROM #{table};")[0][0]
+      end
+
       def self.table
         Tails.to_underscore name
       end
